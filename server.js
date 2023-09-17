dea904f6ba898f8809552abc11e6f50638e7a988
bubbybumble <bubbybumble@gmail.com>
2023-09-17 16:37:40 -0400

    Loading data from JSON file

--- server.js
+++ server.js
@@ -4,27 +4,15 @@ const port = 3000;
 
 app.use('/', express.static('public'));
 
-const budget = {myBudget: [
-  {
-    title: 'Eat Out',
-    budget: 30
-  },
-  {
-    title: 'Groceries',
-    budget: 90
-  },
-  {
-    title: 'Rent',
-    budget: 350
-  },
-]};
+
 
 app.get('/hello', (req, res) => {
   res.send('Hello, world!');
 });
 
 app.get('/budget', (req, res) => {
-  res.json(budget);
+
+  res.json( require(__dirname + '/chart-data.json') );
 });
 
 app.listen(port, () => {
