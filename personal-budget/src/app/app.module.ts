fd11756bc5f8857a5d83d5ef84c791d9d9befc34
bubbybumble <bubbybumble@gmail.com>
2023-09-23 13:50:08 -0400

    init components

--- personal-budget/src/app/app.module.ts
+++ personal-budget/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { HeroComponent } from './hero/hero.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { ArticleComponent } from './article/article.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    HeroComponent,
+    FooterComponent,
+    HomepageComponent,
+    ArticleComponent
   ],
   imports: [
     BrowserModule,
